5657eaaba6a97f69c2274ef9e9783ea8f36aa80c
Mike O'Brien <mikeob9@gmail.com>
2024-10-08 15:55:40 -0400

    build GHA API call

---
+++ payload_sender.R
@@ -0,0 +1,25 @@
+library(httr2)
+
+resp <- 'https://api.github.com/repos/mhpob/biotelemetry-dash-gha/actions/workflows/ingest.yaml/dispatches' |> 
+    request() |> 
+    req_headers(
+        Accept = "application/vnd.github+json",
+        Authorization = "Bearer <ADD TOKEN HERE!!!!>",
+        `X-GitHub-Api-Version` = "2022-11-28",
+        .redact = "Authorization"
+    ) |> 
+    req_body_json(
+        list(
+            ref = "main",
+            inputs = list(
+                p = paste0("*450281.0#20[0009],OK,#9A",
+        "450281,238,2024-10-07 20:13:49,STS,",
+        "DC=1235,PC=88607,LV=0.0,BV=3.5,BU=20.8,I=2.4,T=22.8,DU=0.1,RU=4.1,",
+        "XYZ=-0.06:-0.28:-0.81,#7F",
+        "450281,239,2024-10-07 20:02:07,A69-9001,6277,#A8",
+        "450281,240,2024-10-07 20:07:12,A69-9001,6278,123,#A1>"
+                )
+            )
+        )
+    ) |> 
+    req_perform()
--- test_payloads.R
+++
@@ -1,3 +0,0 @@
-library(httr2)
-
-'https://api.github.com/repos/mhpob/biotelemetry-dash-gha/actions/workflows/WORKFLOW_ID/dispatches'
